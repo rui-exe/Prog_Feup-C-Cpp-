d5ede5369fb5919b9bd07ed2ba6adbf3b5a35c2b
Rui <79858336+rui-exe@users.noreply.github.com>
2021-03-01 15:56:44 +0000

    Add files via upload

---
+++ ficha1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cout <<"Please, input 3 integer numbers: \n";
+    cout << "A: ";
+    cin >> a;
+    cout<<"B: ";
+    cin >> b;
+    cout<<"C: ";
+    cin >> c;
+    double mean;
+    mean = (double(a+b+c))/3;
+    cout << "Mean = "<<mean<<endl;
+    cout << "A-mean = " << a - mean << endl;
+    cout<<"B-mean = " << b-mean << endl;
+    cout<<"C-mean = " << c-mean << endl;
+    return 0;
+}

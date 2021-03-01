eef87ddcf7248ab100e9483b35b8e8fde00171e4
Rui <79858336+rui-exe@users.noreply.github.com>
2021-03-01 15:55:07 +0000

    Add files via upload

---
+++ ficha1/main.cpp
@@ -0,0 +1,11 @@
+/*@author- Rui Pires @feup */
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout << "Enter a character to see it's ascii value:";
+    cin >> c;
+    cout <<"The ascii value of the character " <<c <<" is: " <<int(c)<<endl;
+    return 0;
+}

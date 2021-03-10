b6acf3012d39b586c45c6d9e1eee1a9ae14e627a
Rui <79858336+rui-exe@users.noreply.github.com>
2021-03-10 16:27:46 +0000

    Add files via upload

---
+++ ficha1/2.7b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int num,root;
+    for (int n=1;n<=1000;n++) {
+        bool is_prime = true;
+        for (int i = 2; i <= (int) sqrt(n); i++) {
+            if (n % i == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+        if (n == 0 || n == 1) {
+            cout << "the number " << n << " is not a valid prime number!" << endl;
+        } else if (is_prime) {
+            cout << "the number " << n << " is prime!" << endl;
+        } else {
+            cout << "the number " << n << " is not prime!" << endl;
+        }
+    }
+    return 0;
+    }
+

60659cd6a5e4288aa720a13e1a76c883c839ca3c
Rui <79858336+rui-exe@users.noreply.github.com>
2021-03-10 14:45:19 +0000

    Add files via upload

---
+++ ficha1/2.4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double weight,cost;
+    cout <<"What's the weight of the merchandise:";
+    cin >> weight;
+    double aux = weight;
+    if (weight<= 500)
+    {
+        cost= 5;
+    }
+    else if (weight> 500 && weight<=1000){
+        cost = 5 ;
+        aux -= 500;
+        while (aux>0)
+        {
+            cost += 1.5;
+            aux -= 100;
+        }
+    }
+    else {
+        cost = 12.5;
+        aux -= 1000;
+        while (aux > 0)
+        {
+            cost += 5;
+            aux -= 250;
+        }
+    }
+    cout<<"The cost of transporting "<<weight<<" grams of merchandise is equal to "<<cost<<"$"<<endl;
+    return 0;
+}

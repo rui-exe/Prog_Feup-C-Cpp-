d2248123ee094ed00c252eebd665840d8ae7cb76
Rui <79858336+rui-exe@users.noreply.github.com>
2021-03-14 21:15:15 +0000

    Add files via upload

---
+++ ficha1/2.10.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+bool is_prime(int num)
+{
+    int root;
+    bool prime = true;
+    for (int i=2;i <= (int)sqrt(num);i++)
+    {
+        if (num%i == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+    return prime;
+}
+int main()
+{
+int number,divisor = 2;
+cout<<"Enter a number to see it factorized:";
+cin>>number;
+cout<<"Here's the number factorized:"<<endl;
+while (true)
+{
+    if (is_prime(number))
+    {
+        cout << number;
+        break;
+    }
+    else if (is_prime(divisor) && number%divisor==0)
+    {
+        cout<<divisor<<"x";
+        number = number/divisor ;
+    }
+    else
+        {
+        divisor++;
+    }
+}
+return 0;
+}
+

4ed856a6c57343a229b260e3f88d180cf815b025
Rui <79858336+rui-exe@users.noreply.github.com>
2021-03-15 17:57:57 +0000

    Add files via upload

---
+++ ficha1/3.04.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+double round(double x, unsigned n)
+{
+    int multiplier = pow(10,n);
+    return floor(x * multiplier + 0.5) / multiplier;
+}
+
+int main()
+{
+    double number;
+    unsigned places;
+    cout << "Enter a number to see it rounded: ";
+    cin >> number;
+    cout << "How many decimal places?";
+    cin >> places;
+    cout << "The number " << number << " rounded to " << places << " decimal places is " << round(number, places);
+    return 0;
+}
+

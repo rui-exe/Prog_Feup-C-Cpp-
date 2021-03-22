d99fd39867769dbb5188e6d1f5fa4875b99aa2c9
Rui <79858336+rui-exe@users.noreply.github.com>
2021-03-22 19:42:35 +0000

    Add files via upload

---
+++ ficha1/3.10b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+unsigned long long factorial_rec(unsigned int n){
+    if (n<=1){
+        return 1;
+    }
+    else{
+        return n * factorial_rec(n-1);
+    }
+}
+
+int main(){
+    unsigned int num;
+    cout<<"Number:";
+    cin>>num;
+    cout<<num<<"! = "<<factorial_rec(num)<<endl;
+    return 0;
+}
